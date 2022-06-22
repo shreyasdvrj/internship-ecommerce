a82e28754b1523d395a821c42d4cdcf50f1a9e5f
Meenakshi-10 <54108125+Meenakshi-10@users.noreply.github.com>
2022-06-22 19:23:32 +0530

    Update topBar.js
    Changing css class name to avoid overlap with header css

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -5,8 +5,8 @@ class TopBar extends React.Component {
   render() {
     return (
       <div class="tb-head">
-        <header class="brand-navigation">
-          <div class="content">
+        <header class="tb-brand-navigation">
+          <div class="tb-content">
             <h1 className="tb-h1">{this.props.name}</h1>
             <h2 className="tb-h2">{this.props.value}</h2>
           </div>
