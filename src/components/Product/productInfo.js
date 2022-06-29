6839463abccafb272210c8c6db2f1a1cb320c0be
Meenakshi-10 <54108125+Meenakshi-10@users.noreply.github.com>
2022-06-29 16:02:09 +0530

    Added Button groups

--- src/components/Product/productInfo.js
+++ src/components/Product/productInfo.js
@@ -1,10 +1,13 @@
 import React from "react";
 import './productInfo.css';
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
-import Button from 'react-bootstrap/Button'
+import ButtonGroup from "./buttonGroup";
 
+const printButtonLabel = (event) => {
+    console.log(event.target.name);
+}
 class ProductInfo extends React.Component {
     render() {
+
         return (
             <div className='pdiv'>
                 <h1 className="book-name">{this.props.name}</h1>
@@ -12,15 +15,18 @@ class ProductInfo extends React.Component {
                 <p className="book-desc">{this.props.price}</p>
                 <p className="book-desc">{this.props.desc}</p>
                 <h2 className="book-formats">Formats</h2>
-                <div style={{ 'display': 'flex' }}> 
-                
-                <button className="p-b1">Paperback</button>
-                    <button className="p-b2">Hardcover</button>
-                    <button className="p-b3">Kindle</button>
-               
-                    
+                <div style={{ 'display': 'flex' }}>
+
+
+                    <ButtonGroup
+                        buttons={["Paperback", "Hardcover", "Kindle"]}
+                        afterClick={printButtonLabel} />
+
+
                 </div>
                 <button className="p-addToCart">Add to Cart</button>
+                
+                
             </div>
 
 
