55966e5e4e01de1305744e6e573fdf7d5745b1e8
Meenakshi-10 <54108125+Meenakshi-10@users.noreply.github.com>
2022-07-20 23:38:14 +0530

    Working Filters
    Filters work but need to make it more modular

--- backend/controller/BookCtrl.js
+++ backend/controller/BookCtrl.js
@@ -1,5 +1,11 @@
 const Books = require("../model/BookModel");
 
+// function handleDuplicateKeys(key){
+//   var value;
+//   if(Array.isArray(key))
+//   value = key[key.length-1]
+//   return value;
+// }
 
 const bookCtrl = {
   getBookById: async (req, res) => {
@@ -39,6 +45,7 @@ const bookCtrl = {
     }
   },
   getNonFictionBooks: async (req, res) => {
+    
     try {
       const book = await Books.find({fiction: 'false'}).limit(6);
       res.json(book);
@@ -46,11 +53,29 @@ const bookCtrl = {
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
+
+
   },
   getBookByQuery: async (req, res) => {
     var query = req.query;
+    var science = query.science;
+    var adventure_fantasy = query.adventure_fantasy;
+    var literature = query.literature;
+    if(Array.isArray(science))
+       science = science[science.length-1]
+    if(Array.isArray(adventure_fantasy))
+       adventure_fantasy = adventure_fantasy[adventure_fantasy.length-1]
+    if(Array.isArray(literature))
+       literature = literature[literature.length-1]
+    
+    
     try {
-      const book = await Books.find(query).limit(6);
+      const book = await Books.find( {$or: [{adventure_fantasy:adventure_fantasy},
+        {literature:literature}, {children:query.children}, 
+        {spirituality_religion:query.spirituality_religion}, {education:query.education},
+        {science:science}, {romance:query.romance}, {history:query.history},
+        {comedy:query.comedy}, {knowledge:query.knowledge}, {selfHelp:query.selfHelp},
+        {biographies:query.biographies}]} ).limit(6);
       res.json(book);
       
 
--- src/components/Filters/genreFilters.js
+++ src/components/Filters/genreFilters.js
@@ -37,12 +37,19 @@ function GenreFilters({getFilter}) {
             <div><p className="filterHeading">Categories</p>
       
                 <MDBCheckbox name='genres' value='adventure_fantasy' label='Fantasy' onChange={handleChange}/>
-                <MDBCheckbox name='genres' value='romance'  label='Romance' onChange={handleChange}/>
-                <MDBCheckbox name='genres' value='history'  label='Historical' onChange={handleChange}/>
-                <MDBCheckbox name='genres' value='science'  label='Science' onChange={handleChange}/>
-                <MDBCheckbox name='genres' value='comedy'  label='Comedy' onChange={handleChange}/>
+                <MDBCheckbox name='genres' value='biographies' label='Biographies' onChange={handleChange}/>
                 <MDBCheckbox name='genres' value='children'  label='Children' onChange={handleChange}/>
+                <MDBCheckbox name='genres' value='comedy'  label='Comedy' onChange={handleChange}/>
                 <MDBCheckbox name='genres' value='education'  label='Education' onChange={handleChange}/>
+                <MDBCheckbox name='genres' value='history'  label='Historical' onChange={handleChange}/>
+                <MDBCheckbox name='genres' value='knowledge'  label='Knowledge' onChange={handleChange}/>
+                <MDBCheckbox name='genres' value='literature'  label='Literature' onChange={handleChange}/>
+                <MDBCheckbox name='genres' value='romance'  label='Romance' onChange={handleChange}/>
+                <MDBCheckbox name='genres' value='science'  label='Science' onChange={handleChange}/>
+                <MDBCheckbox name='genres' value='selfHelp'  label='Self Help' onChange={handleChange}/>
+                <MDBCheckbox name='genres' value='spirituality_religion'  label='Spirituality and Religion' onChange={handleChange}/>
+                
+                
                 <button onClick={() => getFilter(query)}>Apply Filters</button>
 
             </div>
